161630e6be711ed450ad8f24b22c319f7f7f23a9
SupiDani <daniel@explorins.com>
2022-09-15 16:19:40 +0100

    [TASK] add ivy wallet params

---
+++ src/entities/Web3.ts
@@ -0,0 +1,9 @@
+
+export interface IvyWalletParams {
+    blockHash: string,
+    chainId: string
+    signedMessage: string
+    smartWallet: string
+    tokenAddress: string
+    transferRedirectUrl: string
+}
